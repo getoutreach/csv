8c2c916d83ccf40e1aced9ff5019f60b8cf57051
Daniel Milde <daniel@milde.cz>
2022-09-01 13:09:31 +0200

    feat: silver lintroller

---
+++ csv.go
@@ -0,0 +1,6 @@
+// Copyright 2022 Outreach Corporation. All Rights Reserved.
+
+// Description: CSV package forked from the standard encoding/csv package without CRLF handling
+
+// Package csv is forked from the standard encoding/csv package without CRLF handling
+package csv
